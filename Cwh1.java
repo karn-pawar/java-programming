9a6f3034846dbe4ebd51161898030689d28f86d1
Karn Pawar <90704668+Kp-killer@users.noreply.github.com>
2021-12-13 00:55:25 +0530

    Update Cwh1.java
    Array Addition in java

--- Cwh1.java
+++ Cwh1.java
@@ -2,7 +2,8 @@ public class Cwh1{
     public static void main(String[]args){
         int[] marks ={100,90,80,60,85};
         int sum=0;
-        for(int i=0;i<=marks.length;i++){
+        for(int element;marks){
+            sum=sum+element;
             {
                 System.out.println(sum);
             }
