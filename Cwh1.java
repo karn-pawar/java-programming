8237d2d10869d920134a9633e96d4405081e2b70
Karn Pawar <90704668+Kp-killer@users.noreply.github.com>
2021-12-13 00:52:44 +0530

    Add files via upload

---
+++ Cwh1.java
@@ -0,0 +1,11 @@
+public class Cwh1{
+    public static void main(String[]args){
+        int[] marks ={100,90,80,60,85};
+        int sum=0;
+        for(int i=0;i<=marks.length;i++){
+            {
+                System.out.println(sum);
+            }
+        }
+    }
+}
